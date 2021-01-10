f36d314d83ba4ab76777f40b4a5c04b08496afb0
Al <alexxmatei@gmail.com>
2021-01-10 07:29:46 +0200

    Added console output to launched (main) application.
    Added a new private method "MessageConsole" for finding (or creating) a
    console with a given name.
    Code adapted from the following page:
    https://wiki.eclipse.org/FAQ_How_do_I_write_to_the_console_from_a_plug-in%3F
    
    Using this new method, a new console named Job-Notifier is created at
    the start of the plugin.
    
    Once installed, the plugin will now print each started and finished job
    in the Job-Notifier console.

--- src/job_notifier/Activator.java
+++ src/job_notifier/Activator.java
@@ -1,38 +1,55 @@
 package job_notifier;
 
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.IConsoleManager;
+import org.eclipse.ui.console.MessageConsole;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 import org.eclipse.core.runtime.jobs.IJobChangeListener;
 import org.eclipse.core.runtime.jobs.Job;
 import job_notifier.handlers.JobNotifier;
 
-
 /**
  * The activator class controls the plug-in life cycle
  */
 public class Activator extends AbstractUIPlugin {
-	
+
 	private IJobChangeListener myJobListener;
-	
+	public static MessageConsole myConsole;
+
 	// The plug-in ID
 	public static final String PLUGIN_ID = "Job-Notifier"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
-	
+
 	/**
 	 * The constructor
 	 */
 	public Activator() {
 	}
 
+	private static MessageConsole findConsole(String name) {
+		ConsolePlugin consolePlugin = ConsolePlugin.getDefault();
+		IConsoleManager conMan = consolePlugin.getConsoleManager();
+		IConsole[] existing = conMan.getConsoles();
+		for (int i = 0; i < existing.length; i++)
+			if (name.equals(existing[i].getName()))
+				return (MessageConsole) existing[i];
+		// no console found, so create a new one
+		MessageConsole messageConsole = new MessageConsole(name, null);
+		conMan.addConsoles(new IConsole[] { messageConsole });
+		return messageConsole;
+	}
+
 	@Override
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
-		
-		this.myJobListener = new JobNotifier();
 
+		this.myJobListener = new JobNotifier();
+		Activator.myConsole = findConsole("Job-Notifier");
 		Job.getJobManager().addJobChangeListener(this.myJobListener);
 	}
 
@@ -41,7 +58,7 @@ public class Activator extends AbstractUIPlugin {
 		if (this.myJobListener != null) {
 			Job.getJobManager().removeJobChangeListener(this.myJobListener);
 		}
-		
+
 		plugin = null;
 		super.stop(context);
 	}
--- src/job_notifier/handlers/JobNotifier.java
+++ src/job_notifier/handlers/JobNotifier.java
@@ -5,6 +5,7 @@ package job_notifier.handlers;
 
 import org.eclipse.core.runtime.jobs.IJobChangeEvent;
 import org.eclipse.core.runtime.jobs.IJobChangeListener;
+import org.eclipse.ui.console.MessageConsoleStream;
 
 /**
  * @author Al
@@ -12,11 +13,26 @@ import org.eclipse.core.runtime.jobs.IJobChangeListener;
  */
 public class JobNotifier implements IJobChangeListener {
 
+	private MessageConsoleStream out;
+	
 	@Override
 	public void aboutToRun(IJobChangeEvent event) {
-		// TODO Auto-generated method stub
+
+		/* Debug start */
+		/*
+		 * Write to the main console of the development environment 
+		 * (not the console of the launched application)
+		 */
 		String jobName = event.getJob().getName();
 		System.out.println("Started job: " + jobName);
+		/* Debug end */
+
+		/*
+		 * Write to the Job-Notifier console of the Eclipse instance 
+		 * that has the Job-Notifier plugin installed
+		 */
+		this.out = job_notifier.Activator.myConsole.newMessageStream();
+		this.out.println("Started job: " + jobName);
 	}
 
 	@Override
@@ -27,9 +43,22 @@ public class JobNotifier implements IJobChangeListener {
 
 	@Override
 	public void done(IJobChangeEvent event) {
-		// TODO Auto-generated method stub
+
+		/* Debug start */
+		/*
+		 * Write to the main console of the development environment 
+		 * (not the console of the launched application)
+		 */
 		String jobName = event.getJob().getName();
 		System.out.println("Finished job: " + jobName);
+		/* Debug end */
+
+		/*
+		 * Write to the Job-Notifier console of the Eclipse instance 
+		 * that has the Job-Notifier plugin installed
+		 */
+		this.out = job_notifier.Activator.myConsole.newMessageStream();
+		this.out.println("Finished job: " + jobName);
 	}
 
 	@Override
@@ -49,5 +78,4 @@ public class JobNotifier implements IJobChangeListener {
 		// TODO Auto-generated method stub
 		
 	}
-
 }
