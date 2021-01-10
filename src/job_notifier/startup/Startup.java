05d6b28884e99032062dc319b501808be05922a5
Al <alexxmatei@gmail.com>
2021-01-10 06:16:59 +0200

    Added "org.eclipse.ui.startup" extension & Startup class.

---
+++ src/job_notifier/startup/Startup.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package job_notifier.startup;
+
+import org.eclipse.ui.IStartup;
+
+/**
+ * @author Al
+ *
+ */
+public class Startup implements IStartup {
+
+	@Override
+	public void earlyStartup() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
